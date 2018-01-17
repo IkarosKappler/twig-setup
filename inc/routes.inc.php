4151323d0a53af24a54dcaaa7acef9ceb7cb5e04
Ikaros Kappler <info@int2byte.de>
2018-01-17 19:10:53 +0100

    Added some resources. Added a routes.php file. Added a parse_request function file.

--- inc/routes.inc.php
+++ inc/routes.inc.php
@@ -3,7 +3,7 @@
 // Define the routes.
 $routes  = array(
     '/'     => array( 'template' => 'index.twig',
-                      'params' => array() ), 
+                      'params' => array() ), // 'bannertext' => '<img src="/img/title-mkpixel-ooh-that-thing-has-numbers-on-it.svg" alt="Oooh, that thing has numbers on it." width="630" height="36" />') ), 
     '/blog' => array( 'template' => 'blog.twig',
                       'params' => array() ), 
     '/blog/blog.20150422_my_basic_html_template' => array( 'template' => 'blog.20150422_my_basic_html_template.twig',
