042f3f64c8301e627135d0d4042f6816d93f507f
ikaroskappler <ikaros.kappler@gmx.de>
2018-03-03 19:14:06 +0100

    Added a random string script. Fixed some headlines.

--- inc/routes.inc.php
+++ inc/routes.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 // Define the routes.
 $routes  = array(
     '/'     => array( 'template' => 'index.twig',
@@ -8,64 +9,64 @@ $routes  = array(
                       'params' => array()
     ),
     '/blog/blog.20161028_Create_Zip_files_omitting_DS_Store_files' => array( 'template' => 'blog.20161028_Create_Zip_files_omitting_DS_Store_files.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Create ZIP files omitting DS_Store files' )
     ),
     '/blog/blog.20161028_Setup_Vagrant_Homestead' => array( 'template' => 'blog.20161028_Setup_Vagrant_Homestead.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Setup Vagrant Homestead' )
     ),
     '/blog/blog.20161025_Vegane_Sucuk' => array( 'template' => 'blog.20161025_Vegane_Sucuk.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Vegane Sucuk' )
     ),
     '/blog/blog.20160412_Remove_checkout_title_from_woocommerce' => array( 'template' => 'blog.20160412_Remove_checkout_title_from_woocommerce.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Remove checkout title from woocommerce' )
     ),
     
     '/blog/blog.20160129_Three.js_Basic_Scene_Setup' => array( 'template' => 'blog.20160129_Three.js_Basic_Scene_Setup.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Three.js Basic Scene Setup' )
     ),
     '/blog/blog.20160129_Gender_Select_Options' => array( 'template' => 'blog.20160129_Gender_Select_Options.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Gender Select Options' )
     ),
     '/blog/blog.20160129_Gender_Select_Options' => array( 'template' => 'blog.20160129_Gender_Select_Options.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Gender Select Options' )
     ),
     '/blog/blog.20160129_Custom_styling_for_ordered_lists_with_CSS3' => array( 'template' => 'blog.20160129_Custom_styling_for_ordered_lists_with_CSS3.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Custom styling for ordered lists with CSS3' )
     ),
     '/blog/blog.20151117_Javascript_Minimizer_Bash_Script' => array( 'template' => 'blog.20151117_Javascript_Minimizer_Bash_Script.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Javascript Minimizer Bash Script' )
     ),
     '/blog/blog.20151026_Setting_up_GPG' => array( 'template' => 'blog.20151026_Setting_up_GPG.twig',
-                                                   'params' => array( 'bannertext' => 'Good news, everyone',
+                                                   'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                       'headline' => 'Setting up GPG' )
     ),
     '/blog/blog.20150422_my_basic_html_template' => array( 'template' => 'blog.20150422_my_basic_html_template.twig',
-                                                   'params' => array( 'bannertext' => 'Good news, everyone',
+                                                   'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                       'headline' => 'My Basic HTML Template' )
     ),
     '/blog/blog.20150326_Booting_my_PC_remotely_using_a_RaspberryPi' => array( 'template' => 'blog.20150326_Booting_my_PC_remotely_using_a_RaspberryPI.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Booting my PC remotely using a Raspberry PI' )
     ),
     '/blog/blog.20141102_Howto_setup_your_GPG' => array( 'template' => 'blog.20141102.Howto_setup_your_GPG.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'How to setup your GnuPG (GPG) keys' )
     ),
     '/blog/blog.20140929_Howto_Fix_shellshock_on_debian_squeeze' => array( 'template' => 'blog.20140929_Howto_Fix_shellshock_on_debian_squeeze.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'How to fix shellshock on Debian Squeeze' )
     ),
     '/blog/blog.20130203_raspi_wlan' => array( 'template' => 'blog.20130203_raspi_wlan.twig',
-                                                             'params' => array( 'bannertext' => 'Good news, everyone',
+                                                             'params' => array( 'bannertext' => 'Good news, everyone!',
                                                                                 'headline' => 'Setting up wireless networking on my Raspberry Pi' )
     )
 );	
