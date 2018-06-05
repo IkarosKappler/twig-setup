36c10f8430e9a73e3efa05ecdf944720e14577ae
ikaroskappler <ikaros.kappler@gmx.de>
2018-06-06 00:26:28 +0200

    Added Expiration-Dates to files via htaccess. Fixed the sitemap.

--- public/index.php
+++ public/index.php
@@ -11,9 +11,8 @@ $request = parse_request();
 require_once '../vendor/autoload.php';
 
 
-//$__urlparts = parse_url(site_url());
-//$__domain = $__urlparts['host'];
 $__domain = $_SERVER['HTTP_HOST'];
+$__protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 
 
 // Set path to templates
@@ -27,12 +26,12 @@ $twig->addFunction(new Twig\TwigFunction('env','_env'));
 if( array_key_exists($request['path'],$routes) ) {
     $route = $routes[$request['path']];
 	echo $twig->render( $route['template'], // 'index.twig',
-                         array_merge( $route['params'], array('__domain' => $__domain) )
+                         array_merge( $route['params'], array('__domain' => $__domain, '__protocol' => $__protocol ) )
 	);
 } else if( $request['path'] == '/sitemap.txt' ) {
     header( 'Content-Type: text/plain' );
     echo $twig->render( 'sitemap.twig',
-                        array( 'routes' => $routes )
+                        array_merge( array( 'routes' => $routes ), array('__domain' => $__domain, '__protocol' => $__protocol ) )
 	);
 } else {
 	echo $twig->render( 'error.twig', array( 'code' => 404, 'message' => 'Not found.' ) );
